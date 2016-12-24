5c50cbb1c4b0fdea006b841a50f26dc979f30978
Zattri <daninjakidy@gmail.com>
2016-12-24 21:34:35 +0000

    Made the combat loop and basic combat

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
 import construct
 
-# Tournament AI game
-
-# Construct Class -
-# Make the AI Class
-#   Name?
-#   HP
-#   Attack
-#   Defence
-#   Speed ?
-#   Agility?
-#   Crit?
-#   Alive state
-#   Winner?
-
-# Functions -
-#   Get Stats
-#   Get Alive
-#   Deal damage
-#   Get turn order?
-
-# Stats setup -
-# Ability points given on generation
-# Spend ability points randomly?
-# Possibly base values then add / subtract some random values
-
-# Tournament setup -
-#   1v1 fights?
-#   Multi-player fights?
-#   Each attack hits a target
-#   Target takes damage based off of attack - def etc etc
-
 dave = construct.Fighter("Dave", 10)
 dave.setAll(10, 3, 6, 5, 4, 2)
-dave.printStats()
+
+bob = construct.Fighter("Bob", 10)
+bob.setAll(10, 4, 5, 3, 4, 1)
+
+# Gameloop
+arena = []
+arena.append(dave)
+arena.append(bob)
+
+battle = True
+p1 = arena[0]
+p2 = arena[-1]
+# Fight loop
+while battle == True:
+    if p1.getAlive() == True and p2.getAlive() == True:
+        p1.printStats()
+        p2.printStats()
+        if (p1.calcInit() >= p2.calcInit()):
+            p2.takeDamage(p1.calcDamage(), p2.calcArmour())
+            p2.updateAlive()
+            if p2.getAlive() == False:
+                p1.takeDamage(p2.calcDamage(), p1.calcArmour())
+        else:
+            p1.takeDamage(p2.calcDamage(), p1.calcArmour())
+            if p1.getAlive() == False:
+                p2.takeDamage(p1.calcDamage(), p2.calcArmour())
+    else:
+        break
+print("A winner has been crowned")
