e12b618f2fdea268de7d5d5953c020d897a0dfea
Zattri <daninjakidy@gmail.com>
2016-12-24 20:57:54 +0000

    Added in new constructor functions

--- construct.py
+++ construct.py
@@ -1,9 +1,12 @@
+import math
+
 class Fighter:
 
     def __init__(self, givenName, statPoints):
             self.name = str(givenName)
             self.hp = -1
             self.atk = 0
+            self.str = 0
             self.defc = 0
             self.spd = 0
             self.agi = 0
@@ -13,32 +16,84 @@ class Fighter:
 #==============================================================================
 # Fetch Commands
 
-    def getName():
-        return self.name
+    def getName(self):
+        return str(self.name)
 
-    def getHP():
-        return self.hp
+    def getHP(self):
+        return int(self.hp)
 
-    def getAtk():
-        return self.atk
+    def getAtk(self):
+        return int(self.atk)
 
-    def getDef():
-        return self.defc
+    def getStr(self):
+        return int(self.str)
 
-    def getSpd():
-        return self.spd
+    def getDef(self):
+        return int(self.defc)
 
-    def getAgi():
-        return self.agi
+    def getSpd(self):
+        return int(self.spd)
 
-    def getAlive():
+    def getAgi(self):
+        return int(self.agi)
+
+    def getAlive(self):
         return self.alive
 
 #==============================================================================
+# Set Commands
 
-#Print Commands
-    def printStats():
-        # Finish later
-        print(self.name, "finish adding stats")
+    def setAll(self, hp, atk, strn, defc, spd, agi):
+        self.hp = hp
+        self.atk = atk
+        self.str = strn
+        self.defc = defc
+        self.spd = spd
+        self.agi = agi
+
+    def setHP(self, value):
+        self.hp = value
+
+    def setAtk(self, value):
+        self.atk = value
+
+    def setStr(self, value):
+        self.str = value
+
+    def setDef(self, value):
+        self.defc = value
+
+    def setSpd(self, value):
+        self.spd = value
+
+    def setAgi(self, value):
+        self.agi = value
+
+    def setAlive(self, value):
+        if int(value) == 1:
+            self.alive = True
+        else:
+            self.alive = False
 
 #==============================================================================
+#Print Commands
+
+    def printStats(self):
+        # Finish later
+        print(self.name, "\nHP:", self.hp, "\nAtk:", self.atk, "Str:", self.str, "Def:", self.defc, "Spd:", self.spd, "Agi:", self.agi)
+
+#==============================================================================
+#Game Commands
+
+    # Take HP damage from attack input and armour value
+    def takeDamage(self, attack, armour):
+        damage = attack - armour
+        self.hp -= damage
+
+    # Calculate the damage to be dealt
+    def calcDamage(self):
+        damage = getAtk() + getStr()
+
+    # Calculate the armour to use for resisting damage
+    def calcArmour(self):
+        armour = math.floor(self.defc / 2)
